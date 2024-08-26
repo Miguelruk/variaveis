c0008654711ba0797a9acf70b57451ab34feb818
Miguelruk <127845073+Miguelruk@users.noreply.github.com>
2024-08-26 23:04:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const nomeCompleto = "Miguel de Mello Bomrruk";
+const anoNascimento = 2006;
+let anoAtual = 2024;
+let idade = 17;
+const anoFormatura = 2028;
+
+console.log("olá eu sou " +  nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " anos " + " e nasci no ano de " + anoNascimento); 
+
+idade = idade + 1;
+anoAtual = anoAtual + 1;
+console.log("Eu vou fazer " + idade + " anos, ano que vem, em " + anoAtual + ". Quero estar cursando engenharia de software, e pretendo me formar em " + anoFormatura);
+
+
+//Agência de viagens Hawaii Paris China Russia Cuba
+const listaDeDestinos = new Array(
+  "hawaii",
+  "russia",
+  "china",
+  "Paris",
+  "cuba"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
+console.log(listaDeDestinos);
+listaDeDestinos.push("Campo Mourão");
+console.log*(listaDeDestinos);
+listaDeDestinos.push("Ponta grossa");
+console.log(listaDeDestinos);
+listaDeDestinos.push("Cabo Frio");
+console.log(listaDeDestinos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
